49b17f24f376254f4cd3aa358cc03f06fb67732d
= <mathivadhani156@gmail.com>
2023-11-14 21:27:39 +0530

    updated

--- src/main/java/com/onesoft/demopro/DemoProController.java
+++ src/main/java/com/onesoft/demopro/DemoProController.java
@@ -1,6 +1,7 @@
 package com.onesoft.demopro;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -8,5 +9,9 @@ public class DemoProController {
 	@GetMapping(value = "/hii")
 	public String getMsg() {
 		return "Good Morning";
+	}
+	@GetMapping("msg/{a}/{b}")
+	public int getadd(@PathVariable int a, @PathVariable int b) {
+		return a+b;
 	}
 }
